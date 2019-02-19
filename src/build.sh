07b790ac54cd7f5b423105de5a84a871b8df4313
Oliver <oliver_marsh@live.com.au>
2019-02-19 12:21:57 +1030

    yea

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 ../bin/ctime -begin test.ctm
 
 ERRORS_OFF=-Wno-c++11-compat-deprecated-writable-strings
-clang++ $ERRORS_OFF -Wl,-rpath,@executable_path/ -g -I ../libs/gl3w -I ../shared/  main.cpp ../libs/gl3w/GL/gl3w.cpp -L../bin -F../bin -framework SDL2 -framework OpenGl -framework CoreFoundation -DDEVELOPER_MODE=1 -DDESKTOP=1 -o ../bin/game 
+clang++ $ERRORS_OFF -Wl,-rpath,@executable_path/ -O2 -I ../libs/gl3w -I ../shared/  main.cpp ../libs/gl3w/GL/gl3w.cpp -L../bin -F../bin -framework SDL2 -framework OpenGl -framework CoreFoundation -DDEVELOPER_MODE=0 -DDESKTOP=1 -o ../bin/game 
 ../bin/ctime -end test.ctm
